b4fc707bb238e52e301dca49ba30d01ca93cd50f
Aleks Rutins <aleks@rutins.com>
2023-01-27 06:48:18 -0500

    Use Tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :opticle, OpticleWeb.Endpoint,
   secret_key_base: "hQEIu5AX8s6YGqvBA3iumXAXvCNUh+PDeQMsp2HUl4Cv8Pto8uyhQRtx7D7ZYSIq",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
